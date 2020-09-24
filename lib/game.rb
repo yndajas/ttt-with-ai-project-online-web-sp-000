af5c738ad27ceee12d8473be1a5d48f645d24540
Ynda Jas <yndajas@gmail.com>
2020-09-24 21:43:35 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Game
   attr_accessor :board, :player_1, :player_2
   WIN_COMBINATIONS = [
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,6 +5,7 @@ module Players
     def move(board)
       cell = 1
       9.times do
+        binding.pry
         return cell.to_s if board.valid_move?(cell)
       end
     end
