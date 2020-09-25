99dad22a3738ee29e1e080d825ee6562697a4fd6
Ynda Jas <yndajas@gmail.com>
2020-09-25 19:14:25 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 module Players
   class Computer < Player
     def move(board)
+      binding.pry
       self.win(board) || self.block(board) || self.ffork(board) || self.block_fork(board) || self.center(board) || self.opposite_corner(board) || self.corner(board) || self.side(board)
     end
     
@@ -30,6 +33,58 @@ module Players
       else
         self.win(board, "X")
       end
+    end
+    
+    def winnable(board, opponent_token)
+      winnable = []
+      
+      Game::WIN_COMBINATIONS.each do |combination|
+        winnable << combination if !combination.collect {|index|board.position(index + 1)}.include?(opponent_token)
+      end
+      
+      winnable
+    end
+    
+    def started(board, token, combinations)
+      started = []
+      
+      combinations.each do |combination|
+        combination.find do |index|
+          started << combination if board.position(index + 1) == token
+        end
+      end
+      
+      started
+    end
+    
+    def valid_moves(board, combinations)
+      valid_moves = []
+
+      started.each do |combination|
+        combination.each do |index|
+          valid_moves << index if board.valid_move?(index + 1)
+        end
+      end
+      
+      valid_moves
+    end
+
+    def fork_moves(board, token = self.token)
+      token == "X" ? opponent_token = "O" : opponent_token = "X"
+      
+      winnable = self.winnable(board, opponent_token) #check which combinations don't include opponent's token
+      
+      started = self.started(board, token, winnable) #check which combinations (of those winnable) do include own token
+      
+      valid_moves = self.valid_moves(board, started) #check which cells are empty in started/winnable combinations
+      
+      
+      # get any fork_moves if one exists (valid moves that appear in more than one winnable and started combinations)
+      
+      fork_moves = valid_moves.select {|move| valid_moves.count(move) > 1}
+      
+      fork_moves.length > 0 ? fork_moves : nil
+      
     end
 
     def ffork(board, token = self.token)
@@ -75,8 +130,8 @@ module Players
       # this is not quite right for blocking forks - it can't work out how to make a move that blocks two forks instead of one that blocks one (it just finds the first fork)
       # if multiple forks, create two in a row to force defence
       # shift code in #ffork to #forks and change so that is selects all fork moves with count > 1 (add .uniq after filtering by counts)
-      # change #ffork to take the [0] (or random) from #forks 
-      # change #block_fork so that if #forks has more than one option, it looks for one in #winnable combinations that has an adjacent cell with the same token (maybe iterate through the forks, select the winnable combinations (may need to separate that out into another method) that include that fork move, then if [1] is token return [0] (or [2]), else return [1])
+      # change #ffork to take the [0] (or random) from #forks
+      # change #block_fork so it prioritises a fork that will force defence (i.e. a fork on a #started combination)
       # this still doesn't seem like it would force a draw if player two plays corner or side after player one plays center
       if self.token == "X"
         self.ffork(board, "O")
